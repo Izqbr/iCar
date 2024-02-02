46428a65af47a483442c52956b550a534014b6cf
Izqbr <alf_84@internet.ru>
2024-02-02 15:26:10 +0400

    edit

--- lib/widgets/refresh_screen.dart
+++ lib/widgets/refresh_screen.dart
@@ -7,10 +7,31 @@ class RefVariant extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,     
+        backgroundColor: const Color.fromARGB(255, 92, 166, 226),
+        title: const Text(
+                      "iCar",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 26,
+                      ),
+                    ),
+        actions: <Widget>[
+          IconButton(
+            onPressed: (){}, 
+            icon: const Icon(Icons.power_settings_new,size: 40, color: Color.fromARGB(255, 255, 255, 255),)
+          )
+
+        ],           
+      
+        ),
         body: RefreshIndicator(
           onRefresh: () async {
-            await Future.delayed(const Duration(seconds: 1));
+            await Future.delayed(const Duration(seconds: 3));
           },
           child: CustomScrollView(
             slivers: <Widget>[
